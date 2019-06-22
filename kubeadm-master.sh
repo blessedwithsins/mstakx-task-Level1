1b776bb208f49528131d26ff7e88d29981c4cd09
blessedwithsins <uchihamadara1503@gmail.com>
2019-06-22 09:50:30 +0530

    Update and rename kubeadm-master to kubeadm-master.sh

--- kubeadm-master.sh
+++ kubeadm-master.sh
@@ -12,7 +12,4 @@ sudo apt-mark hold docker-ce kubelet kubeadm kubectl
 echo "net.bridge.bridge-nf-call-iptables=1" | sudo tee -a /etc/sysctl.conf 
 sudo sysctl -p
 sudo kubeadm init --pod-network-cidr=10.244.0.0/16
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8681ece4de4c0d86c5cd2643275/Documentation/kube-flannel.yml
+
