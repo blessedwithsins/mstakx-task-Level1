7b2a4554901d0575f295ed5579bc0482297c6d54
blessedwithsins <uchihamadara1503@gmail.com>
2019-06-22 12:53:40 +0530

    Update kubeadm-install-master.sh

--- kubeadm-install-master.sh
+++ kubeadm-install-master.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#Install kubeadm, kubectl & docker
+
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
@@ -11,5 +14,31 @@ sudo apt-get install -y docker-ce=18.06.1~ce~3-0~ubuntu kubelet=1.13.5-00 kubead
 sudo apt-mark hold docker-ce kubelet kubeadm kubectl
 echo "net.bridge.bridge-nf-call-iptables=1" | sudo tee -a /etc/sysctl.conf 
 sudo sysctl -p
-sudo kubeadm init --pod-network-cidr=10.244.0.0/16
+
+#Initialize cluster
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --ignore-preflight-errors=numCPU
+
+#Steps to run kubectl as normal user
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+#Apply Network Plugin
+
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8681ece4de4c0d86c5cd2643275/Documentation/kube-flannel.yml
+
+#Create Namespaces
+
+kubectl create ns staging
+kubectl create ns production
+
+#Deploy Ingress Controller
+
+
+#Deploying application in Staging/Production namespaces
+
+git clone https://github.com/kubernetes/examples.git
+cd examples
+kubectl -n staging apply -f frontend-*.yaml redis-*.yaml
+kubectl -n production apply -f frontend-*.yaml redis-*.yaml 
 
